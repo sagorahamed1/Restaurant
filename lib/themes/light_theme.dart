10ab5853fd2877fc7dfdf0bbcc5fca579482c839
sagorahamed1 <sagorahammed002@gmail.com>
2024-01-25 10:30:05 +0600

    theme light

--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -1,33 +1,76 @@
-import 'package:flutter/material.dart';
-import 'package:restaurent_kookbags/utils/app_colors.dart';
+import 'dart:html';
 
-import '../utils/dimensions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:restaurent_kookbags/utils/app_colors.dart';
+import 'package:restaurent_kookbags/utils/dimensions.dart';
 
 ThemeData light = ThemeData(
-    scaffoldBackgroundColor: AppColors.bgWhite,
-    // highlightColor: Color(0xFF9B9B9B),
-    hintColor: const Color(0xFF9B9B9B),
-    brightness: Brightness.light,
+      fontFamily: 'Poppins',
+      primaryColor: AppColors.black100,
+      //secondaryHeaderColor: const Color(0xff04B200),
+      brightness: Brightness.light,
+      cardColor: Colors.white,
+      hintColor: Color(0xFF868889),
+      disabledColor: const Color(0xFFBABFC4),
+      shadowColor: Colors.grey[300],
+      pageTransitionsTheme: const PageTransitionsTheme(builders: {
+            TargetPlatform.android: ZoomPageTransitionsBuilder(),
+            // TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
+            // TargetPlatform.fuchsia: ZoomPageTransitionsBuilder(),
+      }),
+      dividerColor: Color(0xFFDBD5D5),
+      useMaterial3: true,
 
+      elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20.r),
+                  backgroundColor: AppColors.red,
+                  foregroundColor: AppColors.white,
+                  minimumSize: const Size(double.maxFinite, 56),
+                  shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                  ),
+                  textStyle: const TextStyle(
+                        fontSize: Dimensions.fontSizeLarge,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.white,
+                  ),
+            ),
+      ),
 
+      inputDecorationTheme: InputDecorationTheme(
+            prefixIconColor: AppColors.white,
+            suffixIconColor: AppColors.white,
+            iconColor: AppColors.white,
+            filled: true,
+            fillColor:const Color(0xFFEFECEC),
+            hintStyle: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14.sp,
+                  color: AppColors.white100,
+            ),
+      ),
 
-
-    textTheme: const TextTheme(
-      displayLarge: TextStyle(
-          fontWeight: FontWeight.w300, fontSize: Dimensions.fontSizeDefault),
-      displayMedium: TextStyle(
-          fontWeight: FontWeight.w400, fontSize: Dimensions.fontSizeDefault),
-      displaySmall: TextStyle(
-          fontWeight: FontWeight.w500, fontSize: Dimensions.fontSizeDefault),
-      headlineMedium: TextStyle(
-          fontWeight: FontWeight.w600, fontSize: Dimensions.fontSizeDefault),
-      headlineSmall: TextStyle(
-          fontWeight: FontWeight.w700, fontSize: Dimensions.fontSizeDefault),
-      titleLarge: TextStyle(
-          fontWeight: FontWeight.w800, fontSize: Dimensions.fontSizeDefault),
-      bodySmall: TextStyle(
-          fontWeight: FontWeight.w900, fontSize: Dimensions.fontSizeDefault),
-      titleMedium: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500),
-      bodyMedium: TextStyle(fontSize: 12.0),
-      bodyLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
-    ));
+      appBarTheme: AppBarTheme(
+          backgroundColor: AppColors.bgWhite,
+          centerTitle: true,
+          titleTextStyle: const TextStyle(
+              color: AppColors.black,
+              fontSize: Dimensions.fontSizeExtraLarge,
+              fontWeight: FontWeight.w600
+          ),
+          iconTheme: IconThemeData(
+                color: AppColors.black,
+                size: 23.r,
+          )),
+      scaffoldBackgroundColor: AppColors.bgWhite,
+      dialogBackgroundColor: AppColors.white,
+      menuButtonTheme: const MenuButtonThemeData(),
+      // popupMenuTheme:  const PopupMenuThemeData(
+      //       position: PopupMenuPosition.under,
+      //       color: AppColors.whiteColor,
+      //       surfaceTintColor: AppColors.whiteColor,
+      // ),
+      backgroundColor: Colors.transparent,
+);
