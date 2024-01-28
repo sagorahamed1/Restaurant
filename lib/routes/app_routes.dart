88d59150b344e4abe6a76673d1751f8baecc56e2
swaponahmmed59 <swaponahmmed59@gmail.com>
2024-01-28 10:49:28 +0600

    Some isshu Fixed

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:restaurent_kookbags/view/screens/category/category_screen.dart';
 import 'package:restaurent_kookbags/view/screens/home/home_screen.dart';
+import 'package:restaurent_kookbags/view/screens/log_in/log_in_screen.dart';
 import 'package:restaurent_kookbags/view/screens/otp_next/otp_next_screen.dart';
 import 'package:restaurent_kookbags/view/screens/otp_submit/otp_submit_screen.dart';
 import 'package:restaurent_kookbags/view/screens/otp_verification/otp_verification_screen.dart';
@@ -20,6 +21,7 @@ class AppRoutes{
   static const String otpNextScreen = "/otp_next_screen.dart";
   static const String otpVerificationScreen = "/otp_verification_screen.dart";
   static const String signUpScreen = "/sign_up_screen.dart";
+  static const String logInUpScreen = "/log_in_screen.dart";
   static const String storesDetailsScreen = "/StoresDetailsScreen";
   static const String storesScreen = "/StoresScreen";
 
